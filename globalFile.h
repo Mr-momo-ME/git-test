cc6922f069b3e6ab27129ffa2dfa0509dbb28608
Mr-momo-ME <lwwplayer@163.com>
2023-09-16 10:57:19 +0800

    tasks.json中给test测试任务添加cwd,解决之前测试任务时文件根目录不在fileDirname的问题

--- globalFile.h
+++ globalFile.h
@@ -1,12 +1,12 @@
 #pragma once
 
 // 管理员文件
-#define ADMIN_FILE "../admin.txt"
+#define ADMIN_FILE "./admin.txt"
 // 学生文件
-#define STUDENT_FILE "../student.txt"
+#define STUDENT_FILE "./student.txt"
 // 教师文件
-#define TEACHER_FILE "../teacher.txt"
+#define TEACHER_FILE "./teacher.txt"
 // 机房信息文件
-#define COMPUTER_FILE "../computer.txt"
+#define COMPUTER_FILE "./computer.txt"
 // 预约信息文件
-#define ORDER_FILE "../order.txt"
+#define ORDER_FILE "./order.txt"
